d46bbd18b6637b06050be01b9abde1e562ae8e85
TAJIRI <bsclmr183621@spu.ac.ke>
2025-07-24 10:49:51 +0300

    Added elections app to INSTALLED_APPS

--- student_council/settings.py
+++ student_council/settings.py
@@ -30,14 +30,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our app
+    'elections',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
